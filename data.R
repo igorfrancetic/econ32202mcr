31352d8d52be85aa0d605637a62a4cccb694fc01
Igor Francetic <94715500+igorfrancetic@users.noreply.github.com>
2023-03-04 23:32:55 +0000

    Update data.R

--- data.R
+++ data.R
@@ -6,25 +6,25 @@ setwd(path)
 set.seed(333)
 
 # Generate index of ids
-tutorial4<-matrix(nrow=1000)
-tutorial4<-as.data.frame(tutorial4)
-colnames(tutorial4)<-c("id")
-tutorial4$id<-c(seq(1:1000))
+tutorial5<-matrix(nrow=1000)
+tutorial5<-as.data.frame(tutorial5)
+colnames(tutorial5)<-c("id")
+tutorial5$id<-c(seq(1:1000))
 
 # Simulate characteristics and outcomes
-tutorial4$age<-sample(18:65, 1000, replace=T)
-tutorial4$female<-rbinom(1000,1,0.5)
-tutorial4$nonwhite<-rbinom(1000,1,0.5)
-tutorial4$needsindex<-runif(1000,0,1+(tutorial4$age/65))
-tutorial4$supplyindex<-runif(1000,0,2)
-tutorial4$income<-round(10000+10000*rgamma(1000,3,1),digits=0)
-tutorial4$nhsregime1[tutorial4$income<=20000]<-0.15
-tutorial4$nhsregime1[tutorial4$income>20000&tutorial4$income<=30000]<-0.20
-tutorial4$nhsregime1[tutorial4$income>30000]<-0.30
-tutorial4$nhsregime2[tutorial4$income<=20000]<-0.30
-tutorial4$nhsregime2[tutorial4$income>20000&tutorial4$income<=30000]<-0.25
-tutorial4$nhsregime2[tutorial4$income>30000]<-0.05
-tutorial4$hcaretax1<-tutorial4$income*tutorial4$nhsregime1
-tutorial4$hcaretax2=tutorial4$income*tutorial4$nhsregime2
-tutorial4$gpvisits<-round(rpois(1000,1.5)+0.8*tutorial4$needsindex-0.7*tutorial4$nonwhite+tutorial4$income/1000000+0.5*tutorial4$supplyindex,digits=0)
-save(tutorial4,file="tutorial4.Rda")
+tutorial5$age<-sample(18:65, 1000, replace=T)
+tutorial5$female<-rbinom(1000,1,0.5)
+tutorial5$nonwhite<-rbinom(1000,1,0.5)
+tutorial5$needsindex<-runif(1000,0,1+(tutorial5$age/65))
+tutorial5$supplyindex<-runif(1000,0,2)
+tutorial5$income<-round(10000+10000*rgamma(1000,3,1),digits=0)
+tutorial5$nhsregime1[tutorial5$income<=20000]<-0.15
+tutorial5$nhsregime1[tutorial5$income>20000&tutorial5$income<=30000]<-0.20
+tutorial5$nhsregime1[tutorial5$income>30000]<-0.30
+tutorial5$nhsregime2[tutorial5$income<=20000]<-0.30
+tutorial5$nhsregime2[tutorial5$income>20000&tutorial5$income<=30000]<-0.25
+tutorial5$nhsregime2[tutorial5$income>30000]<-0.05
+tutorial5$hcaretax1<-tutorial5$income*tutorial5$nhsregime1
+tutorial5$hcaretax2=tutorial5$income*tutorial5$nhsregime2
+tutorial5$gpvisits<-round(rpois(1000,1.5)+0.8*tutorial5$needsindex-0.7*tutorial5$nonwhite+tutorial5$income/1000000+0.5*tutorial5$supplyindex,digits=0)
+save(tutorial5,file="tutorial5.Rda")
