b02d0722102d87b70cf7167dc815f657a698c658
Igor Francetic <94715500+igorfrancetic@users.noreply.github.com>
2023-03-05 11:51:29 +0000

    Update data.R

--- data.R
+++ data.R
@@ -26,5 +26,5 @@ tutorial5$nhsregime2[tutorial5$income>20000&tutorial5$income<=30000]<-0.25
 tutorial5$nhsregime2[tutorial5$income>30000]<-0.05
 tutorial5$hcaretax1<-tutorial5$income*tutorial5$nhsregime1
 tutorial5$hcaretax2=tutorial5$income*tutorial5$nhsregime2
-tutorial5$gpvisits<-round(rpois(1000,1.5)+0.8*tutorial5$needsindex-0.7*tutorial5$nonwhite+tutorial5$income/1000000+0.5*tutorial5$supplyindex,digits=0)
+tutorial5$gpvisits<-round(rpois(1000,1.5)+0.8*tutorial5$needsindex-0.7*tutorial5$nonwhite+tutorial5$income/10000+0.5*tutorial5$supplyindex,digits=0)
 save(tutorial5,file="tutorial5.Rda")
